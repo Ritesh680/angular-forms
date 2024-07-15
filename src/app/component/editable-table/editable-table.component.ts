ea89483a530494664196711aa0bc44fa59155fe4
Ritesh Poudel <ritesh.poudel@kotuko.it>
2024-07-15 13:37:29 +0545

    editable table created

--- src/app/component/editable-table/editable-table.component.ts
+++ src/app/component/editable-table/editable-table.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, inject } from '@angular/core';
 import {
+  AbstractControl,
   ControlContainer,
   FormArray,
-  FormBuilder,
   FormControl,
   FormGroup,
   FormGroupDirective,
@@ -11,54 +11,87 @@ import {
 } from '@angular/forms';
 import { ButtonComponent } from '@components/button/button.component';
 import { CheckboxComponent } from '@components/checkbox/checkbox.component';
-import { InputfieldComponent } from '@components/inputfield/inputfield.component';
 import { ModalComponent } from '@components/modal/modal.component';
 import { SelectComponent } from '@components/select/select.component';
 import { TableInputFieldComponent } from '@components/table-input-field/table-input-field.component';
-import { TextareaComponent } from '@components/textarea/textarea.component';
 import { EditableTableHeader } from '@interfaces';
 import { INPUT_FIELD_TYPES } from '../../@types/enums';
+import { TextareaComponent } from '@components/textarea/textarea.component';
 
 @Component({
   selector: 'app-editable-table',
   standalone: true,
   imports: [
-    CommonModule,
     ReactiveFormsModule,
-    InputfieldComponent,
+    CommonModule,
     TableInputFieldComponent,
-    TextareaComponent,
-    ButtonComponent,
     CheckboxComponent,
-    SelectComponent,
     ModalComponent,
+    SelectComponent,
+    ButtonComponent,
+    TextareaComponent,
   ],
   templateUrl: './editable-table.component.html',
   styleUrl: './editable-table.component.css',
   viewProviders: [
-    { provide: ControlContainer, useExisting: FormGroupDirective },
+    {
+      provide: ControlContainer,
+      useFactory: () => inject(FormGroupDirective),
+    },
   ],
 })
-export class EditableTableComponent implements OnInit {
-  @Input() noSN: boolean = false;
-  @Input() tableHeaders: EditableTableHeader[] = [];
-  @Input({ required: true }) name: string = '';
-  @Input() index: number = 0;
+export class EditableTableComponent implements OnInit, OnDestroy {
+  /**
+   * @description
+   * Headers configuration for the editable table
+   * @note
+   * Action column should not be added in the tableHeaders
+   * @type {EditableTableHeader[]}
+   */
+  @Input({ required: true }) tableHeaders!: EditableTableHeader[];
 
-  isModalOpen: boolean = false;
-  modalId: string = '';
+  /**
+   * If the table should not have serial number column
+   */
+  @Input() noSN = false;
 
-  constructor(
-    private fb: FormBuilder,
-    private controlContainer: ControlContainer
-  ) {}
+  /**
+   * Form group for the section containing the table
+   */
+  @Input() section!: AbstractControl;
+  /**
+   * If the table is nested inside form group then set this to true
+   */
+  @Input() isNested = false;
+
+  /**
+   * Name of the table
+   */
+  @Input({ required: true }) name!: string;
+
+  isModalOpen = false;
+  modalId = '';
+
+  constructor(private controlContainer: ControlContainer) {}
 
   get tableForm() {
     return this.controlContainer?.control as FormGroup;
   }
 
+  get formGroup() {
+    return this.section as FormGroup;
+  }
+
   get rowsArray() {
-    return this.tableForm.get(this.name) as FormArray;
+    if (this.isNested) {
+      return this.section.get(this.name) as FormArray;
+    }
+    return this.tableForm?.get(this.name) as FormArray;
+  }
+
+  get allFields() {
+    const fields = this.section?.get(this.name) as FormArray;
+    return fields.controls;
   }
 
   get showModalFor(): EditableTableHeader['key'][] {
@@ -104,19 +137,8 @@ export class EditableTableComponent implements OnInit {
     this.closeModal();
   }
 
-  getSectionFields = () => {
-    const obj: Record<string, FormControl> = {};
-    this.tableHeaders.map(
-      (field) =>
-        (obj[field.key] = new FormControl(
-          field.inputType === INPUT_FIELD_TYPES.CHECKBOX ? false : ''
-        ))
-    );
-    return obj;
-  };
-
   addRow() {
-    const newRow = this.fb.group(this.getSectionFields());
+    const newRow = this.getSectionFields();
 
     this.rowsArray?.push(newRow);
   }
@@ -125,9 +147,68 @@ export class EditableTableComponent implements OnInit {
     this.rowsArray.removeAt(index);
   }
 
+  getSectionFields() {
+    return new FormGroup({
+      name: new FormControl(''),
+      title: new FormControl(''),
+      subtitle: new FormControl(''),
+      showCondition: new FormControl(''),
+      value: new FormControl(''),
+      valueFn: new FormControl(''),
+      type: new FormControl(''),
+      description: new FormControl(''),
+      placeholder: new FormControl(''),
+      mandatory: new FormControl(false),
+      readonly: new FormControl(false),
+      hidden: new FormControl(false),
+      format: new FormControl(''),
+    });
+  }
+
   ngOnInit(): void {
-    if (this.rowsArray?.length === 0) {
-      this.addRow();
+    if (this.isNested && !this.section) {
+      throw new Error('section is required when isNested is true');
+    }
+
+    const fieldGroup = this.getSectionFields();
+
+    if (this.isNested) {
+      const sectionsArray = this.tableForm.get('sections') as FormArray;
+
+      if (sectionsArray) {
+        const sectionIndex = sectionsArray.controls.findIndex(
+          (section) => section === this.section
+        );
+
+        if (sectionIndex !== -1) {
+          const sectionGroup = sectionsArray.at(sectionIndex) as FormGroup;
+          const fieldsArray = sectionGroup.get('fields') as FormArray;
+
+          if (fieldsArray) {
+            fieldsArray.push(fieldGroup);
+          } else {
+            throw new Error(
+              `Fields array not found in section "${this.section}".`
+            );
+          }
+        } else {
+          throw new Error(`Section "${this.section}" not found.`);
+        }
+      } else {
+        throw new Error('Nested form array "sections" not found.');
+      }
+      return;
+    } else {
+      this.tableForm.addControl(this.name, new FormArray([fieldGroup]));
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.isNested) {
+      const nested = this.section as FormGroup;
+      nested.removeControl(this.name);
+    } else {
+      this.tableForm.removeControl(this.name);
     }
   }
 }
--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,33 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 import {
-  AbstractControl,
   FormArray,
   FormBuilder,
-  FormControl,
   FormGroup,
   ReactiveFormsModule,
 } from '@angular/forms';
 import { FIELD_TYPES, MODULES, sectionFields, sections } from '@constants';
-import { DropdownProps, EditableTableHeader } from '@interfaces';
+import { DropdownProps } from '@interfaces';
 import { InputfieldComponent } from '@components/inputfield/inputfield.component';
 import { ButtonComponent } from '@components/button/button.component';
 import { CheckboxComponent } from '@components/checkbox/checkbox.component';
 import { SpinningLoaderComponent } from '@components/spinning-loader/spinning-loader.component';
 import { EditableTableComponent } from '@components/editable-table/editable-table.component';
 import { SelectComponent } from '@components/select/select.component';
-import { SectionFieldTypes } from './template.interface';
+
 import { CardComponent } from '@components/card/card.component';
 import { LabelComponent } from '../../component/label/label.component';
 import { CommonModule } from '@angular/common';
-import { INPUT_FIELD_TYPES } from '../../@types/enums';
-import { ModalComponent } from '@components/modal/modal.component';
+
 import { TextareaComponent } from '@components/textarea/textarea.component';
 import { TableInputFieldComponent } from '@components/table-input-field/table-input-field.component';
-
-interface IObject {
-  [x: string]: FormControl<string | null>;
-}
 
 @Component({
   selector: 'app-template',
@@ -43,7 +36,6 @@ interface IObject {
     SelectComponent,
     CardComponent,
     LabelComponent,
-    ModalComponent,
     TextareaComponent,
     TableInputFieldComponent,
   ],
@@ -66,6 +58,7 @@ export class TemplateComponent implements OnInit {
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       module: '',
+
       sections: this.fb.array([
         this.fb.group({
           name: '',
@@ -82,54 +75,6 @@ export class TemplateComponent implements OnInit {
 
   get sectionArray() {
     return this.form.get('sections') as FormArray;
-  }
-
-  getSectionFields = () => {
-    const obj: IObject = {};
-    sectionFields.map((field) => (obj[field.value] = new FormControl('')));
-    return obj;
-  };
-
-  getSectionFieldsTableHeaders(tableHeaders: SectionFieldTypes[]) {
-    return tableHeaders.map((header) => ({
-      headerLabel: header.label,
-      key: header.value,
-      inputType: header.type,
-      selectOptions: 'selectOptions' in header ? header.selectOptions : [],
-    }));
-  }
-
-  get showModalFor(): EditableTableHeader['key'][] {
-    const showModalsFor: EditableTableHeader['key'][] = [];
-    this.fieldsTableHeaders.forEach((header) =>
-      header.inputType === INPUT_FIELD_TYPES.TEXTAREA ||
-      (header.inputType === INPUT_FIELD_TYPES.SELECT &&
-        header.selectOptions?.length === 0)
-        ? showModalsFor.push(header.key)
-        : null
-    );
-
-    return showModalsFor;
-  }
-
-  isModalOpenFor(index: number, modalId: string): boolean {
-    return this.isModalOpen && this.modalId === `fields.${index}.${modalId}`;
-  }
-
-  openModal(index: number, modalId: string) {
-    this.isModalOpen = true;
-    this.modalId = `fields.${index}.${modalId}`;
-  }
-  setDefaultValueOnModalCancel() {
-    //
-  }
-  handleModalCancel() {
-    this.setDefaultValueOnModalCancel();
-    this.closeModal();
-  }
-
-  getFields(section: AbstractControl) {
-    return section.get('fields') as FormArray;
   }
 
   closeModal() {
@@ -171,19 +116,6 @@ export class TemplateComponent implements OnInit {
         fields: this.fb.array([]),
       })
     );
-
-    this.addRow(this.sectionArray.length - 1);
-  }
-
-  addRow(i: number) {
-    const newRow = this.fb.group(this.getSectionFields());
-    const fields = this.sectionArray.at(i).get('fields') as FormArray;
-    fields.push(newRow);
-  }
-
-  deleteRow(i: number, index: number) {
-    const fields = this.sectionArray.at(i).get('fields') as FormArray;
-    fields.removeAt(index);
   }
 
   ngOnInit(): void {
@@ -191,9 +123,5 @@ export class TemplateComponent implements OnInit {
       label: module,
       value: module.toLowerCase(),
     }));
-
-    if (this.sectionArray.value[0].fields?.length === 0) {
-      this.addRow(0);
-    }
   }
 }
